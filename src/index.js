775f48cf748fa301b29561b38b030e155cf39709
Dominik Eymold <deymold@xiag.ch>
2018-07-31 13:33:44 +0200

    remove global

--- src/index.js
+++ src/index.js
@@ -7,10 +7,8 @@ import store from "./store";
 import { moveTrains, assignTrainToRailway, updatePassengers } from "./actions";
 
 window.store = store;
-window.moveTrains = moveTrains;
 
 const title = 'Coding challenge';
-
 
 const mapStateToProps = state => {
     return state;
